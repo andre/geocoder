5db69a1d67a41956dd899eb25847b33eff28c384
Andre Lewis <andre.lewis@gmail.com>
2018-04-30 22:41:32 -0600

    reorg files

---
+++ src/geocoder/result.cr
@@ -0,0 +1,22 @@
+module Geocoder  
+  class Result
+    getter success : Bool
+    getter lat : Float64
+    getter lng : Float64
+    getter address : String
+    getter all_results 
+
+    def initialize(success, lat = 0.0 , lng = 0.0, address = "")
+      @success = success
+      @lat = lat
+      @lng = lng
+      @address = address
+      @all_results = [] of Result
+    end
+
+    def all_results=(array_of_results)
+      @all_results=array_of_results
+    end    
+
+  end
+end
